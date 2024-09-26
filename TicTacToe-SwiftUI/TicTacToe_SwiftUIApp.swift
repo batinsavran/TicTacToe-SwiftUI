08a95a7bdcf0a7e29a8a6b672b1e5d2a49aa9353
Oğulcan Aslan <batinsavran00@gmail.com>
2024-09-26 10:59:56 +0300

    Initial Commit

---
+++ TicTacToe-SwiftUI/TicTacToe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_SwiftUIApp.swift
+//  TicTacToe-SwiftUI
+//
+//  Created by mobiroller on 26.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
