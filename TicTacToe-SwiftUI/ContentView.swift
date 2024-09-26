494e07a0c31eead938c89bc2b5e3395db9b4b334
Oğulcan Aslan <batinsavran00@gmail.com>
2024-09-26 14:34:29 +0300

    Add ContentView for displaying game grid layout
    Implemented ContentView using SwiftUI to display a 3x3 grid layout. The grid cells contain placeholder text, with adjustable font size, bold styling, and background color. The grid uses VStack and HStack for row and column layout with customizable spacing and padding.

--- TicTacToe-SwiftUI/ContentView.swift
+++ TicTacToe-SwiftUI/ContentView.swift
@@ -1,20 +1,31 @@
-//
-//  ContentView.swift
-//  TicTacToe-SwiftUI
-//
-//  Created by mobiroller on 26.09.2024.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    var body: some View 
+    {
+        let borderSize = CGFloat(5)
+        
+        VStack(spacing: borderSize)
+        {
+            ForEach(0...2, id: \.self) 
+            {
+                row in
+                HStack(spacing: borderSize) 
+                {
+                    ForEach(0...2, id: \.self) 
+                    {
+                        column in
+                        Text("X")
+                            .font(.system(size: 60))
+                            .bold()
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+                            .aspectRatio(1, contentMode: .fit)
+                            .background(Color.white)
+                    }
+                }
+            }
         }
+        .background(Color.black)
         .padding()
     }
 }
