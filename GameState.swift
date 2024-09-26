3aa147e768a9451ab18f6e9a51fce4ada893bf99
Oğulcan Aslan <batinsavran00@gmail.com>
2024-09-26 14:55:39 +0300

    Enhance GameState with turn management and scoring
    Updated GameState to support alternating turns between Cross and Nought players. Added scoring logic for both players and incorporated victory detection (to be implemented in the future). Included logic for placing tiles, resetting the board, and triggering alerts for game outcomes such as draws and victories.

--- GameState.swift
+++ GameState.swift
@@ -2,25 +2,60 @@ import Foundation
 
 class GameState: ObservableObject
 {
-    @Published var board = [[Cell]]()  // 2D array of Cell objects
+    @Published var board = [[Cell]]()
+    @Published var turn = Tile.Cross
+    @Published var noughtsScore = 0
+    @Published var crossesScore = 0
+    @Published var showAlert = false
+    @Published var alertMessage = "Draw"
+    
     
     init()
     {
         resetBoard()
     }
     
+    func placeTile(_ row: Int,_ column: Int)
+    {
+        if(board[row][column].tile != Tile.Empty)
+        {
+            return
+        }
+        board[row][column].tile = turn == Tile.Cross ? Tile.Cross : Tile.Nought
+        
+        if (chechForVictory())
+        {
+            if(turn == Tile.Cross)
+            {
+                crossesScore += 1
+            }
+            else
+            {
+                noughtsScore += 1
+            }
+        }
+        else
+        {
+            turn = turn == Tile.Cross ? Tile.Nought : Tile.Cross
+        }
+    }
+    
+    func chechForVictory() -> Bool {
+        return false
+    }
+    
     func resetBoard()
     {
-        var newBoard = [[Cell]]()  // Creates an empty 2D array for the board
+        var newBoard = [[Cell]]()
         for _ in 0...2
         {
-            var row = [Cell]()  // Should be a single-dimensional array of Cell, not [[Cell]]
+            var row = [Cell]()
             for _ in 0...2
             {
-                row.append(Cell(tile: Tile.Empty))  // Appends a Cell with Tile.Empty to the row
+                row.append(Cell(tile: Tile.Empty))
             }
-            newBoard.append(row)  // Appends the row to the newBoard
+            newBoard.append(row)
         }
-        board = newBoard  // Assign the newBoard to the board
+        board = newBoard
     }
 }
